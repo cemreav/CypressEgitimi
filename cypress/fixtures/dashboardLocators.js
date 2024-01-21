3cf621979c561bebcbbec94812d376b44287fd05
cemreavcc@gmail.com <cemreavcc@gmail.com>
2024-01-21 11:41:01 +0300

    added space char

--- cypress/fixtures/dashboardLocators.js
+++ cypress/fixtures/dashboardLocators.js
@@ -6,5 +6,6 @@ export default{
   paymentText:"//div[@class='summary_info_label'][text()='Payment Information']",
   shippingText:"//div[@class='summary_info_label'][text()='Shipping Information']",
   priceText:"//div[@class='summary_info_label'][text()='Price Total']",
-  finishText:"//h2[text()='Thank you for your order!']"
+  finishText:"//h2[text()='Thank you for your order!']",
+  
 }
